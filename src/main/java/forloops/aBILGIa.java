9daef710bd84cb19555821a2f23acb8d8166b6da
hanzelkaraagac <hanzelgamess@gmail.com>
2022-10-23 20:17:30 +0300

    For Loops Sorular

---
+++ src/main/java/forloops/aBILGIa.java
@@ -0,0 +1,84 @@
+package forloops;
+
+public class aBILGIa {
+
+    /*
+
+    Döngüler, belirli bir koşul sağlandığı sürece tekrarlanması gereken işler için kullanılan
+programlama kodlarıdır. Döngü deyimleri, bahsedilen komut parçalarının belirtilen şartlar
+gerçekleştikçe tekrar tekrar işlenmesini sağlar. Java'da for, while ve do-while , for-each olmak üzere
+4 adet döngü deyimi bulunmaktadır.
+
+Kısacası : Döngü yapıları kodumuzun belirli kısımlarının döngüye girmesini ve birden fazla
+kez çalışmasını sağlar. Döngüler bir koşula bağlanır ve bu koşul var olduğu sürece aynı kod
+bloğu çalışmaya devam eder. Koşul artık sağlanmıyorsa döngü sona erer.
+
+
+while: Java programla dilinde while döngüleri yapacağımız işin ne zaman biteceğiniz bilmediğimiz
+ durumlarda kullanışlı olabilmektedir.
+
+
+ ******************************************************
+
+
+
+*****************************
+Do-While Döngüsü
+Do-while döngüsünün çalışma mantığı while döngüsü ile aynıdır , fakat while döngüsünde
+parantez içerisindeki koşul sağlandığı sürece kod bloğu çalışacaktır. Ama Do-while
+döngüsünde durum false olsa bile döngü en az bir kere çalışacaktır. Bu tür durumlar
+için Do-while kullanılır.
+
+
+
+*********************************************
+
+
+Java While ve Do-While Arasındaki Farklar ?
+Java'da Do-While ve While döngüleri arasındaki tek fark , Do-while döngüsünde, döngü bloğu
+içindeki kod kesimi en az bir kez mutlaka işletilecektir. Çünkü önce döngü bloğu işletilip
+sonra koşul denetlenmektedir. While döngüsünde ise önce koşula bakılıp sonra döngü bloğu
+işletildiği için, döngüye hiç girilmemesi olasıdır.
+
+***************************************************
+
+
+
+For Döngüsü
+Java'da döngüler konusunda öğrendiğimiz gibi For döngüsü ile bir koşul belirterek
+döngülerimizi oluşturabiliriz.
+
+
+
+
+for (int i = 0; i < 10; i++) {
+  System.out.println(i);
+}
+
+Bu döngüler uygun şekilde tasarlandıkları sürece aynı işi yapabilirler. Ama genellikle, kaç kere döneceği belli olmayan,
+ koşulun bir girdiye göre denetlendiği durumlarda While ya da do-while döngüsü kullanılırken, diziler gibi,
+  tekrar sayısı belirli olan durumlarda ise for döngüsü kullanılması tercih edilir. Ama yine bu yazılımcının
+  kendisine bağlıdır.
+
+  *******************************************
+
+
+
+
+  For-Each Döngüsü
+
+  dizilerdeki ve listelerdeki elemanları daha hızlı şekilde ulaşmak için kullanılan kısa bir yöntemdir.
+  Kısacası for döngüsünün modifiye edilmiş halidir.
+
+  for (veritipi degisken: diziAdi) {
+    // kod bloğu
+
+
+
+
+
+
+     */
+
+
+}
